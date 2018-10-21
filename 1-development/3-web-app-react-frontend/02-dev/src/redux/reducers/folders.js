81e418732f6d4a8a17d8c3db882872747c79e63b
Warren R Bank <warren.r.bank@gmail.com>
2018-10-21 02:46:40 -0700

    in-progress

--- 1-development/3-web-app-react-frontend/02-dev/src/redux/reducers/folders.js
+++ 1-development/3-web-app-react-frontend/02-dev/src/redux/reducers/folders.js
@@ -9,6 +9,20 @@ const RDCR = {}
 RDCR['REPLACE'] = (state, {folders}) => {
   if (!folders || !Array.isArray(folders) || !folders.length) return state  // noop
 
+  // catch unneeded updates
+  {
+    let no_change = (state.length === folders.length)
+
+    for (let i=0; no_change && (i < state.length); i++) {
+      no_change = (
+       (state[i].folder_name  === folders[i].folder_name) &&
+       (state[i].unread_count === folders[i].unread_count)
+      )
+    }
+
+    if (no_change) return state  // noop
+  }
+
   const new_state = folders.map(folder => {return {...folder}})
   return new_state
 }
