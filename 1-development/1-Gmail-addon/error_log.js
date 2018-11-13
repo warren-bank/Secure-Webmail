16e8f4fc8845061efda25f21e5205e8953778aad
Warren R Bank <warren.r.bank@gmail.com>
2018-11-12 19:25:29 -0800

    in-progress

--- 1-development/1-Gmail-addon/error_log.js
+++ 1-development/1-Gmail-addon/error_log.js
@@ -1,6 +1,33 @@
+function get_error_string_chunks(e) {
+  try {
+    var props = Object.getOwnPropertyNames(e)
+    var i, prop
+    var str = ''
+
+    str += '{'
+    for (i=0; i < props.length; i++) {
+      prop = props[i]
+      if (i>0) str += ', '
+      str += '"' + prop + '": ' + JSON.stringify(e[prop])
+    }
+    str += '}'
+
+    // prevent error: "Your input contains more than the maximum of 50000 characters in a single cell."
+    var chunks = str.match(/.{1,50000}/g)
+    return chunks
+  }
+  catch(err){
+    return [e.message, err.message]
+  }
+}
+
 function log_error(ss, e) {
-  var range = ss.getRange(ss.getLastRow()+1, 1, 1, 3)
-  var values = [[new Date(), user.email, e]]
+  var string_chunks = get_error_string_chunks(e)
+  var last_column_index = 2 + string_chunks.length
+  var row_data = [new Date(), state.email_address].concat(string_chunks)
+
+  var range = ss.getRange(ss.getLastRow()+1, 1, 1, last_column_index)
+  var values = [row_data]
   range.setValues(values)
 }
 
