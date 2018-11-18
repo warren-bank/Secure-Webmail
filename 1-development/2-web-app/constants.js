479b290c9617f722cd23c530849479c11220a100
Warren R Bank <warren.r.bank@gmail.com>
2018-11-18 11:09:32 -0800

    in-progress

--- 1-development/2-web-app/constants.js
+++ 1-development/2-web-app/constants.js
@@ -1,6 +1,7 @@
 var app = {
-  title:   'Secure Webmail',
-  version: '0.1.0'
+  title:      'Secure Webmail',
+  version:    '0.1.0',
+  public_url: 'https://script.google.com/macros/s/AKfycbxjGr_DXhEsXfQxDtKII3LkCEGC5zgsQcdx4DJNWJitkyx8FLZV/exec'
 }
 
 var spreadsheet_id = {
