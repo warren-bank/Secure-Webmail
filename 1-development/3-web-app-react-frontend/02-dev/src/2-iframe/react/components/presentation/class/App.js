warren-bank/Secure-Webmail

commit 53ff7b50675c1afb935a3540dc5c936863b867d0

Google OAuth App Verification: add README to login page
* Google Cloud Platform/API Trust & Safety:
  * OAuth App Verification request
    * status: "Request Denied"
      * pending appeal
  * reasons:
    * The information you provided reveals that the following homepage/privacy policy URL is not reachable and/or looks like a test URL.
    * It appears that your homepage URL is a login screen, which restricts public access to your app's information and intent.
    * If your app is meant to be public, please ensure that your app's homepage URL features the following:
      1. Content associated with your brand
      2. Accurately represents your true identity
      3. Clearly describes the functionality of your app

1-development/3-web-app-react-frontend/02-dev/dist/2-iframe/css/style.css
@@ -1,10 +1,5 @@
 * {
   box-sizing: border-box;
-
-  -webkit-margin-before: 0px;
-  -webkit-margin-after:  0px;
-  -webkit-margin-start:  0px;
-  -webkit-margin-end:    0px;
 }
 
 html,
@@ -12,11 +7,27 @@ body,
 .wrapper,
 #root,
 #root > .app,
-#root > .app > .header,
 #root > .app > .iframe_container,
-#root > .app > .iframe_container > iframe {
+#root > .app > .iframe_container > iframe,
+#root > .app > .header,
+#root > .app > .summary_container,
+#root > .app > .summary_container > .summary,
+#root > .app > .summary_container > .summary > .github_ribbon,
+#root > .app > .summary_container > .summary > .README,
+#root > .app > .summary_container > .summary > .policies {
   margin: 0;
   padding: 0;
+
+  -webkit-margin-before: 0px;
+  -webkit-margin-after:  0px;
+  -webkit-margin-start:  0px;
+  -webkit-margin-end:    0px;
+}
+
+/* ------------------------------------------------------------------ */
+
+#root > .app > .iframe_container > iframe {
+  min-height: 500px;  /* required for initial Google user authorization prompt */
 }
 
 /* ------------------------------------------------------------------ */
@@ -70,8 +81,72 @@ body,
 
 /* ------------------------------------------------------------------ */
 
-#root > .app > .iframe_container > iframe {
-  min-height: 500px;  /* required for initial Google user authorization prompt */
+
+#root > .app > .summary_container > .summary {
+  width: 90%;
+  margin: 1em auto;
+}
+
+#root > .app > .summary_container > .summary {
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+  font-size: 16px;
+  line-height: 1.5;
+  word-wrap: break-word;
+}
+
+#root > .app > .summary_container > .summary h3 {
+  font-size: 1.3em;
+  font-weight: bold;
+}
+#root > .app > .summary_container > .summary h4 {
+  font-size: 1.15em;
+  font-weight: bold;
+}
+#root > .app > .summary_container > .summary a {
+  color: #0366d6;
+  text-decoration: none;
+}
+#root > .app > .summary_container > .summary a:hover {
+  text-decoration: underline;
+}
+#root > .app > .summary_container > .summary code {
+  background-color: rgba(27,31,35,.05);
+  border-radius: 3px;
+  font-size: 85%;
+  margin: 0;
+  padding: .2em .4em;
+  font-family: SFMono-Regular,Consolas,Liberation Mono,Menlo,Courier,monospace;
+}
+#root > .app > .summary_container > .summary img {
+  max-width: 100%;
+}
+#root > .app > .summary_container > .summary hr {
+  margin: 0.5em 0;
+}
+#root > .app > .summary_container > .summary li {
+  line-height: 1.75em;
+}
+
+/* ------------------------------------------------------------------ */
+
+#root > .app > .summary_container > .summary > .github_ribbon {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+#root > .app > .summary_container > .summary > .github_ribbon,
+#root > .app > .summary_container > .summary > .github_ribbon > a > img {
+  width: 149px;
+  height: 149px;
+}
+
+/* ------------------------------------------------------------------ */
+
+#root > .app > .summary_container > .summary,
+#root > .app > .summary_container > .summary > .README > h3:first-child {
+  margin-top: 0;
+  padding-top: 0;
 }
 
 /* ------------------------------------------------------------------ */

1-development/3-web-app-react-frontend/02-dev/src/2-iframe/react/components/presentation/class/App.js
@@ -1,6 +1,9 @@
 const React       = require('react')
 const PropTypes   = require('prop-types')
 
+const displayName = 'App'
+
+const Summary                             = require(`./${displayName}/Summary`)
 const {GoogleLogin, GoogleLogout}         = require('react-google-login')
 
 const {getQuerystring, removeQuerystring} = require('react/lib/querystring')
@@ -64,13 +67,21 @@ class App extends React.Component {
     }
 
     return (
-      <div className="app">
+      <div className={displayName.toLowerCase()}>
       {
         !this.state.account &&
         <div className="header">
           {this.buttons.login}
         </div>
       }
+      {
+        !this.state.account &&
+        <div className="summary_container">
+          <Summary />
+        </div>
+      }
+      {
+        this.state.account &&
         <div className="iframe_container">
           <iframe
             id={this.props.iframe_id}
@@ -82,6 +93,7 @@ class App extends React.Component {
             scrolling="no"
           ></iframe>
         </div>
+      }
       </div>
     )
   }

commit afd6c8481984ef2009a83e5b89fba629d64eea9c

fix bug introduced by recent commit (53ff7b5)

1-development/3-web-app-react-frontend/02-dev/dist/2-iframe/css/style.css
@@ -27,7 +27,43 @@ body,
 /* ------------------------------------------------------------------ */
 
 #root > .app > .iframe_container > iframe {
-  min-height: 500px;  /* required for initial Google user authorization prompt */
+  min-height: 500px;
+}
+
+/* *******************************
+ * When the user (re)loads the page, React state is lost.
+ * However, within the iframe, the Google Apps Script can determine the user account (#0),
+ * and communicate its email address to the parent window.
+ * In order for this communication to occur, the iframe needs to be rendered alongside the login screen.
+ *
+ * When the user is already logged into one (or more) Google account(s),
+ * React receives the user's email address from the iframe and adds it to its state.
+ * This triggers a fresh render, which removes the login screen.
+ *
+ * When the user is not logged into any Google account, the login screen is rendered.
+ * For the reasons just explained, the iframe must also be added to the DOM.
+ * Currently:
+ *  - Google redirects the page inside the iframe to a login page
+ *  - the server response for this login page prevents its rendering within an iframe
+ *  - the iframe appears empty
+ * However:
+ *  - should Google ever fix this error and the iframe properly display a login prompt:
+ *     - on the one hand, this page could be dramatically simplified since Google website sign-in would no-longer be necessary
+ *     - on the other hand, this page would look horrible until I got around to making changes
+ *  - to prevent the latter,
+ *    the following CSS rule is intended to hide the iframe in the DOM when the login screen is visible
+ *     - a communication channel will still exist
+ *     - any content within the iframe is hidden
+ * *******************************
+ */
+#root > .app > .iframe_container.hidden {
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
 }
 
 /* ------------------------------------------------------------------ */

1-development/3-web-app-react-frontend/02-dev/src/2-iframe/react/components/presentation/class/App.js
@@ -80,9 +80,7 @@ class App extends React.Component {
           <Summary />
         </div>
       }
-      {
-        this.state.account &&
-        <div className="iframe_container">
+        <div className={`iframe_container ${this.state.account ? '' : 'hidden'}`}>
           <iframe
             id={this.props.iframe_id}
             src={url}
@@ -93,7 +91,6 @@ class App extends React.Component {
             scrolling="no"
           ></iframe>
         </div>
-      }
       </div>
     )
   }